fix masking of hot pixels: use intensities up to 1e9 cts per image

image_processing_ioc.py
@@ -40,8 +40,8 @@ def beam_analysis(imageData: np.ndarray, ROI_SIZE: int) -> Union[tuple, float]:
     while imageData.ndim > 2:
         imageData = np.sum(imageData, axis=0)
 
-    # Step 2: get rid of masked or pegged pixels on an Eiger detector
-    labeled_foreground = (np.logical_and(imageData >= 0, imageData <= 1e6)).astype(int)
+    # Step 1: get rid of masked or pegged pixels on an Eiger detector
+    labeled_foreground = (np.logical_and(imageData >= 0, imageData <= 1e9)).astype(int)
     maskedTwoDImage = imageData * labeled_foreground  # apply mask
     threshold_value = np.maximum(
         1, 0.0001 * maskedTwoDImage.max()